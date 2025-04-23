ef29378959e741a9097e50de6d4ce62c204d157c
AirinNila <airinakhternila>
2025-04-23 12:47:56 +0600

    finish project

--- script/play.js
+++ script/play.js
@@ -7,8 +7,77 @@ function continueGame(){
 
 // play
 function forPlay(){
-    const playNow = hideElementById('home')
-    const nextStep =showElementById('play-ground')
+    hideElementById('home')
+    hideElementById('final-score')
+    showElementById('play-ground')
+
+    // reset value
+    setElementValue('current-life', 5);
+    setElementValue('current-score', 0)
     continueGame()
 }
+
+// for key down
+
+// for key down
+
+function haldlerKeyPress (event){
+    const playerPressed = event.key;
+    const playerKey = document.getElementById('current-alphabet');
+    const getKey = playerKey.innerText;
+    const expectedKey = getKey.toLowerCase();
+
+    if( playerPressed === 'escape'){
+        gameOver()
+    }
+    
+    if(playerPressed === expectedKey){
+        continueGame()
+        removeBgColor(expectedKey)
+        //update score
+        // const showScore = document.getElementById('current-score');
+        // const getScore = showScore.innerText;
+        // const convertCurrentScore = parseInt(getScore);
+
+        const currentScore = increseOrdecreseNumber('current-score')
+        // new score
+        const newScore = currentScore + 1;
+        const setTheValue = setElementValue('current-score', newScore)
+
+    }
+    else{
+        // const getCurrentLifeId = document.getElementById('current-life');
+        // const getCurrentLife = getCurrentLifeId.innerText;
+        // const convertCurrentLife = parseInt(getCurrentLife);
+
+        const currentLife = increseOrdecreseNumber('current-life')
+
+        const newLife = currentLife - 1 ;
+        const updatedLife = setElementValue('current-life', newLife)
+
+       if(newLife === 0){
+            gameOver();
+       }
+    }
+}
+
+document.addEventListener('keyup', haldlerKeyPress)
+
+// game over
+function gameOver(){
+    const hidePlayGround = hideElementById('play-ground')
+    const showResult = showElementById ('final-score')
+    const lastScore = increseOrdecreseNumber('current-score')
+    setElementValue('show-result', lastScore)
+
+    // remove bg
+    const currentAlphabet = getAlpha('current-alphabet')
+    removeBgColor(currentAlphabet)
+}
+
+// play again
+// function playAgain(){
+//     const hideresult = hideElementById('final-score');
+//     const showResult = showElementById('play-ground')
+// }
 
--- script/utillity.js
+++ script/utillity.js
@@ -28,3 +28,33 @@ function setBgColorById(id){
     const element = document.getElementById(id);
     element.classList.add('bg-[#FFA500]')
 }
+
+// remove background by id
+
+function removeBgColor(id){
+    const element = document.getElementById(id);
+    element.classList.remove('bg-[#FFA500]')
+}
+
+// for convert to number 
+
+function increseOrdecreseNumber (id){
+    const getId = document.getElementById(id);
+    const value = getId.innerText;
+    const convert = parseInt(value);
+    return convert;
+}
+
+// set element value 
+
+function setElementValue (element, value){
+    const getElement = document.getElementById(element);
+    getElement.innerText = value;
+}
+
+// for get alphabet
+function getAlpha(id){
+    const element = document.getElementById(id);
+    const text = element.innerText;
+    return text;
+}
